1e921f078c0478627123d1a47b961021209b884c
Szymon Maniak <szymekmaniak075@gmail.com>
2024-09-20 20:56:16 +0200

    gotowe

--- 06_INF.03-2024-01.04-SG/rozwiazanie/kalendarz.php
+++ 06_INF.03-2024-01.04-SG/rozwiazanie/kalendarz.php
@@ -7,20 +7,68 @@
     <title>Zadania na lipiec</title>
 </head>
 <body>
-    <div id="baner_1">
+    <header id="baner_1">
         <img src="logo1.png" alt="lipiec">
-    </div>
-    <div id="baner_2">
+    </header>
+    <header id="baner_2">
         <h1>TERMINARZ</h1>
-        <p>najbliższe zadania:</p>
-    </div>
-    <div style="clear: both;"></div>
-    <div id="glowny">
+        <p>najbliższe zadania:
+            <?php
+                $polaczenie = mysqli_connect('localhost', 'root', '', 'terminarz');
+                if(!$polaczenie){
+                    exit();
+                }
+                else{
+                    $zapytanie = "SELECT DISTINCT wpis FROM `zadania` WHERE dataZadania >= '2020-07-01' AND dataZadania <= '2020-07-07' AND wpis != '';";
 
-    </div>
-    <div id="stopki">
+                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+
+                    $text = "";
+                    while($row = mysqli_fetch_row($odpowiedz)){
+                        $text .= $row[0]."; ";
+                    }
+                    echo $text;
+                }
+                mysqli_close($polaczenie);
+            ?>
+        </p>
+    </header>
+    <div style="clear: both;"></div>
+    <main id="glowny">
+                <?php
+                    $polaczenie = mysqli_connect('localhost', 'root', '', 'terminarz');
+                    if(!$polaczenie){
+                        exit();
+                    }
+                    else{
+                        $zapytanie = "SELECT `dataZadania`, `wpis` FROM `zadania` WHERE miesiac = 'lipiec';";
+
+                        $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+
+                        /*
+                            <div>
+                            <h6>data</h6>
+                            <p>opis</p>
+                            </div>
+                        */
+
+                        $text = "";
+                        while($row = mysqli_fetch_assoc($odpowiedz)){
+                            $text .= '<div>';
+                            $text .= '<h6>'.$row['dataZadania'].'</h6>';
+                            $text .= '<p>'.$row['wpis'].'</p>';
+                            $text .= '</div>';
+                        }
+
+                        echo $text;
+                    }
+                    mysqli_close($polaczenie);
+                ?>
+    </main>
+    <div style="clear: both;"></div>
+    <footer id="stopka">
         <a href="sierpien.html">Terminasz na sierpień</a>
         <p>Stronę wykonał: Szymon Maniak 5TI</p>
-    </div>
+    </footer>
 </body>
 </html>
