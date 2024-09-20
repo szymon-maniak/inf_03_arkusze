95c6273eae287cf4fb10341b9b9d16e60b546549
Szymon Maniak <szymekmaniak075@gmail.com>
2024-09-20 18:21:47 +0200

    zaczynam

---
+++ 06_INF.03-2024-01.04-SG/rozwiazanie/kalendarz.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl6.css">
+    <title>Zadania na lipiec</title>
+</head>
+<body>
+    <div id="baner_1">
+
+    </div>
+    <div id="baner_2">
+
+    </div>
+    <div style="clear: both;"></div>
+    <div id="glowny">
+
+    </div>
+    <div id="stopki">
+
+    </div>
+</body>
+</html>
