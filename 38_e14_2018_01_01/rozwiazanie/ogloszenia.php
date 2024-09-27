5c61e1703ba8e9f8be7ec8e8f5cad0094cfa9907
Szymon Maniak <szymekmaniak075@gmail.com>
2024-09-27 18:55:15 +0200

    php

--- 38_e14_2018_01_01/rozwiazanie/ogloszenia.php
+++ 38_e14_2018_01_01/rozwiazanie/ogloszenia.php
@@ -41,7 +41,28 @@
     </aside>
     <main>
         <h2>Ogłoszenia kategorii książki</h2>
-        <!-- skrypt 1 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'ogloszenia');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie_1 = "SELECT id, tytul, tresc FROM `ogloszenie` WHERE kategoria = 1;";
+                $zapytanie_2 = "SELECT telefon FROM `ogloszenie` JOIN uzytkownik ON ogloszenie.uzytkownik_id = uzytkownik.id;";
+                $odpowiedz_1 = mysqli_query($polaczenie, $zapytanie_1);
+                $odpowiedz_2 = mysqli_query($polaczenie, $zapytanie_2);
+                $text = " ";
+                while($tablica_1 = mysqli_fetch_array($odpowiedz_1)){
+                    $text .= "<h3>".$tablica_1['id']." ".$tablica_1['tytul']."</h3>";
+                    $text .= "<p>".$tablica_1['tresc']."</p>";
+                    if($tablica_2 = mysqli_fetch_array($odpowiedz_2)){
+                        $text .= "<p>"."telefon kontaktowy ".$tablica_2['telefon']."</p>";
+                    }
+                }
+                echo $text;
+                mysqli_close($polaczenie);
+            }
+        ?>
     </main>
     <footer>
         Portal ogłoszeniowy opracował: Szymon Maniak 5TI
