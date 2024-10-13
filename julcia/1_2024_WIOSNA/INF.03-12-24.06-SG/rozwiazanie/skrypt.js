607cbbbc0aae0a4057412b69876bbbb8118d2f7f
Szymon Maniak <szymekmaniak075@gmail.com>
2024-10-13 14:27:25 +0200

    start_js

---
+++ julcia/1_2024_WIOSNA/INF.03-12-24.06-SG/rozwiazanie/skrypt.js
@@ -0,0 +1,24 @@
+function obraz_1(){
+    let pszczola = document.getElementById('pszczola');
+    if(document.getElementById('blur').checked){
+        pszczola.style.filter = "blur(5px)";
+    }
+    else if(document.getElementById('sepia').checked){
+        pszczola.style.filter = "sepia(100%)";
+    }
+    else if(document.getElementById('negatyw').checked){
+        pszczola.style.filter = "invert(100%)";
+    }
+}
+
+function obraz_2(){
+    
+}
+
+function obraz_3(){
+
+}
+
+function obraz_4(){
+
+}
