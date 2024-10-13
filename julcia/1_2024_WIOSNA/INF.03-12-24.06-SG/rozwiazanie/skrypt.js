e867374fa8084f7a38e3bad281ff7587df1c3eae
Szymon Maniak <szymekmaniak075@gmail.com>
2024-10-13 14:44:37 +0200

    js

--- julcia/1_2024_WIOSNA/INF.03-12-24.06-SG/rozwiazanie/skrypt.js
+++ julcia/1_2024_WIOSNA/INF.03-12-24.06-SG/rozwiazanie/skrypt.js
@@ -11,14 +11,26 @@ function obraz_1(){
     }
 }
 
-function obraz_2(){
-    
+function obraz_2(zmienna){
+    let pomarancza = document.getElementById('pomarancza');
+    if(zmienna == 1){
+        pomarancza.style.filter = "grayscale(0%)";
+    }
+    else if(zmienna == 2){
+        pomarancza.style.filter = "grayscale(100%)";
+    }
 }
 
 function obraz_3(){
-
+    let owoce = document.getElementById('owoce');
+    let suwak_owoce = document.getElementById('suwak_owoce').value;
+    let text = "opacity("+ suwak_owoce +"%)";
+    owoce.style.filter = text;
 }
 
 function obraz_4(){
-
+    let zolw = document.getElementById('zolw');
+    let suwak_zolw = document.getElementById('suwak_zolw').value;
+    let text = "brightness("+ suwak_zolw +"%)";
+    zolw.style.filter = text;
 }
