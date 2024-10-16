15e24c16f0a0130fd1ebfb26ab5c4218480b09b2
Szymon Maniak <szymekmaniak075@gmail.com>
2024-10-16 21:37:34 +0200

    php

--- julcia/3_ee09-2020-styczen-egzamin-zawodowy-praktyczny/rozwiazanie/wedkowanie.php
+++ julcia/3_ee09-2020-styczen-egzamin-zawodowy-praktyczny/rozwiazanie/wedkowanie.php
@@ -16,6 +16,22 @@
     <section id="prawy">
         <h3>Ryby spokojnego żeru (białe)</h3>
         <!-- skrypt -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'wedkowanie');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie = "SELECT id, nazwa, wystepowanie FROM ryby WHERE styl_zycia = 2;";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                $text = " ";
+                while($tablica = mysqli_fetch_row($odpowiedz)){
+                    $text .= "$tablica[0]. $tablica[1], występuje w: $tablica[2]<br>";
+                }
+                echo $text;
+            }
+            mysqli_close($polaczenie);
+        ?>
         <ol>
             <li><a href="http://wedkuje.pl/" target="_blank">Odwiedź takie;</a></li>
             <li><a href="http://www.pzw.org.pl/" target="_blank">Polski Związek Wędkarski</a></li>
