1f252479f5fa076d2fe5c09c7beaca9585fe81b0
Szymon Maniak <szymekmaniak075@gmail.com>
2024-10-19 22:22:48 +0200

    php

--- 2024_WIOSNA/INF.03-11-24.06-SG/rozwiazanie/peruwianka.php
+++ 2024_WIOSNA/INF.03-11-24.06-SG/rozwiazanie/peruwianka.php
@@ -19,14 +19,65 @@
         <h3>Poznaj wszystkie rasy świnek morskich</h3>
         <ol>
             <!-- skrypt 1 -->
+            <?php
+                $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
+                if(!$polaczenie){
+                    exit();
+                }
+                else{
+                    $zapytanie = "SELECT rasa FROM `rasy` WHERE 1;";
+                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                    $text = " ";
+                    while($tablica = mysqli_fetch_row($odpowiedz)){
+                        $text .= "<li>$tablica[0]</li>";
+                    }
+                    echo $text;
+                }
+                mysqli_close($polaczenie);
+            ?>
         </ol>
     </section>
     <main>
         <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
         <!-- skrypt 2 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie = "SELECT DISTINCT swinki.data_ur, swinki.miot, rasy.rasa FROM `swinki` JOIN rasy ON swinki.rasy_id = rasy.id WHERE rasy_id LIKE 1;";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                $text = " ";
+                while($tablica = mysqli_fetch_row($odpowiedz)){
+                    $text .= "<h2>Rasa: $tablica[2]</h2>";
+                    $text .= "<p>Data urodzenia: $tablica[0]</p>";
+                    $text .= "<p>Oznaczenie miotu: $tablica[1]</p>";
+                }
+                echo $text;
+            }
+            mysqli_close($polaczenie);
+        ?>
         <hr>
         <h2>Świnki w tym miocie</h2>
         <!-- skrypt 3 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie = "SELECT imie, cena, opis FROM `swinki` WHERE rasy_id LIKE 1;";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                $text = " ";
+                while($tablica = mysqli_fetch_row($odpowiedz)){
+                    $text .= "<h3>$tablica[0] - $tablica[1] zł</h3>";
+                    $text .= "<p>$tablica[2]</p>";
+                }
+                echo $text;
+            }
+            mysqli_close($polaczenie);
+        ?>
     </main>
     <footer>
         <p>Stronę wykonał: Szymon Maniak  5TI</p>
