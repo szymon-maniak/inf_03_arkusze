fe76ec9bcbddcf98847f3901911cf57e9d31fe89
Szymon Maniak <szymekmaniak075@gmail.com>
2024-12-10 21:27:20 +0100

    php

--- ee09_2022_06_03/rozwiazanie/opinie.php
+++ ee09_2022_06_03/rozwiazanie/opinie.php
@@ -13,6 +13,24 @@
     <main>
         <h2>Opinie naszych klientów</h2>
         <!-- skrypt 1 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'hurtownia');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie = "SELECT klienci.zdjecie, klienci.imie, opinie.opinia FROM `klienci` JOIN opinie ON klienci.id = opinie.Klienci_id WHERE klienci.Typy_id IN(2,3);";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                while($tab = mysqli_fetch_array($odpowiedz)){
+                    echo "<div class='opinia'>";
+                        echo "<img src='$tab[0]' alt='klient'>";
+                        echo "<blockquote>$tab[2]</blockquote>";
+                        echo "<h4>$tab[1]</h4>";
+                    echo "</div>";
+                }
+            }
+            mysqli_close($polaczenie);
+        ?>
     </main>
     <footer id="footer1">
         <h3>Współpracują z nami</h3>
@@ -22,6 +40,20 @@
         <h3>Nasi top klienci</h3>
         <ol>
             <!-- skrypt 2 -->
+            <?php
+                $polaczenie  = mysqli_connect('localhost', 'root', '', 'hurtownia');
+                if(!$polaczenie){
+                    exit();
+                }
+                else{
+                    $zapytanie = "SELECT imie , nazwisko, punkty FROM `klienci` ORDER BY punkty DESC LIMIT 3;";
+                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                    while($tab = mysqli_fetch_array($odpowiedz)){
+                        echo "<li>$tab[0] $tab[1], $tab[2]</li>";
+                    }
+                }
+                mysqli_close($polaczenie);
+            ?>
         </ol>
     </footer>
     <footer id="footer3">
