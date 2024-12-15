c90ae66c874f9a7c66eaa6431423528efcba7505
Szymon Maniak <szymekmaniak075@gmail.com>
2024-12-15 13:15:24 +0100

    start robienia walidacji

--- ARKUSZE-DLC/INF.03-08-24.06-SG-MOD/rozwiazanie/skrypt.js
+++ ARKUSZE-DLC/INF.03-08-24.06-SG-MOD/rozwiazanie/skrypt.js
@@ -31,6 +31,21 @@ function zatwierdz(){
     document.getElementById('wynik').innerHTML = imie + ", " + nazwisko + ", " + data_urodzenia + ", " + ulica + ", " + numer + ", " + miasto + ", " + numer_komorkowy + ", " + rodo;
 }
 
+function walidacja(){
+    let imie = document.getElementById('imie').value;
+    let nazwisko = document.getElementById('nazwisko').value;
+    let data_urodzenia = document.getElementById('data_urodzenia').value;
+    let ulica = document.getElementById('ulica').value;
+    let numer = document.getElementById('numer').value;
+    let miasto = document.getElementById('miasto').value;
+    let numer_komorkowy = document.getElementById('numer_komorkowy').value;
+    let rodo = document.getElementById('rodo').value;
+
+    if(!imie == null || !imie == ""){
+        zwiekszPasek();
+    }
+}
+
 let szerokosc = 4;
 function zwiekszPasek(){
     if(szerokosc < 100){
