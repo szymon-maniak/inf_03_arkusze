f17b49ebd6577d7f3a6f58a4e22b73d97a9d1fe1
Szymon Maniak <szymekmaniak075@gmail.com>
2024-12-31 19:48:05 +0100

    funkcja dziala

--- 2024/2024_WIOSNA/inf_03_2024_06_11/funkcja_rozwiazanie/crested.php
+++ 2024/2024_WIOSNA/inf_03_2024_06_11/funkcja_rozwiazanie/crested.php
@@ -19,62 +19,58 @@
         <h3>Poznaj wszystkie rasy świnek morskich</h3>
         <ol>
             <!-- skrypt 1 -->
-            <?php
-                $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
-                if(!$polaczenie){
-                    exit();
-                }
-                else{
-                    $zapytanie = "SELECT rasa FROM `rasy` WHERE 1;";
-                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
-                    while($tab = mysqli_fetch_array($odpowiedz)){
+            <?php 
+                polaczenie(
+                    "SELECT rasa FROM `rasy` WHERE 1;",
+                    function($tab) {
                         echo "<li>$tab[0]</li>";
                     }
-                }
-                mysqli_close($polaczenie);
+                );
             ?>
         </ol>
     </section>
     <section id="glowny">
-        <img src="crested.jpg" alt="Świnka morska rasy crested">
+    <img src="crested.jpg" alt="Świnka morska rasy crested">
         <!-- skrypt 2 -->
         <?php
-            $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
-            if(!$polaczenie){
-                exit();
-            }
-            else{
-                $zapytanie = "SELECT DISTINCT(data_ur), miot, rasy.rasa FROM `swinki` JOIN rasy ON swinki.rasy_id = rasy.id WHERE rasy.id LIKE 7;";
-                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
-                while($tab = mysqli_fetch_array($odpowiedz)){
+            polaczenie(
+                "SELECT DISTINCT(data_ur), miot, rasy.rasa FROM `swinki` JOIN rasy ON swinki.rasy_id = rasy.id WHERE rasy.id LIKE 7;",
+                function($tab) {
                     echo "<h2>Rasa: $tab[2]</h2>";
                     echo "<p>Data urodzenia: $tab[0]</p>";
                     echo "<p>Oznaczenie miotu: $tab[1]</p>";
                 }
-            }
-            mysqli_close($polaczenie);
+            );
         ?>
         <hr>
         <h2>Świnki w tym miocie</h2>
         <!-- skrypt 3 -->
         <?php
-            $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
-            if(!$polaczenie){
-                exit();
-            }
-            else{
-                $zapytanie = "SELECT imie, cena, opis FROM `swinki` WHERE rasy_id LIKE 7;";
-                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
-                while($tab = mysqli_fetch_array($odpowiedz)){
+            polaczenie(
+                "SELECT imie, cena, opis FROM `swinki` WHERE rasy_id LIKE 7;",
+                function($tab) {
                     echo "<h3>$tab[0] - $tab[1] zł</h3>";
                     echo "<p>$tab[2]</p>";
                 }
-            }
-            mysqli_close($polaczenie);
+            );
         ?>
     </section>
     <footer>
         <p>Stronę wykonał: Szymon Maniak 5TI</p>
     </footer>
+    <?php
+        function polaczenie($zapytanie, $callback){
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'hodowla');
+            if (!$polaczenie) {
+                exit();
+            } else {
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                while ($tab = mysqli_fetch_array($odpowiedz)) {
+                    $callback($tab);
+                }
+            }
+            mysqli_close($polaczenie);
+        }
+    ?>
 </body>
 </html>
