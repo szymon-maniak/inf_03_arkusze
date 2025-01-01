064159d58c9ff7f2342a82e47194601280b8a0bd
Szymon Maniak <szymekmaniak075@gmail.com>
2025-01-01 16:46:18 +0100

    php

--- 2024/2024_WIOSNA/inf_03_2024_06_04/rozwiazanie/galeria.php
+++ 2024/2024_WIOSNA/inf_03_2024_06_04/rozwiazanie/galeria.php
@@ -22,10 +22,46 @@
     </section>
     <section id="srodkowy">
         <!-- skrypt 1 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'galeria');
+            if(!$polaczenie){
+                exit();
+            } else{
+                $zapytanie = "SELECT zdjecia.plik, zdjecia.tytul, zdjecia.polubienia, autorzy.imie, autorzy.nazwisko FROM `autorzy` JOIN zdjecia ON autorzy.id = zdjecia.autorzy_id ORDER BY autorzy.nazwisko ASC;";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                while($tab = mysqli_fetch_array($odpowiedz)){
+                    echo "<div class='kafelek'>";
+                    echo "<img src='$tab[0]' alt='zdjęcie'>";
+                    echo "<h3>$tab[1]</h3>";
+                    if($tab[2] > 40){
+                        echo "<p>Autor: $tab[3] $tab[4].<br>Wiele osób polubiło ten obraz</p>";
+                    } else{
+                        echo "<p>Autor: $tab[3] $tab[4]</p>";
+                    }
+                    echo "<a href='$tab[0]' download>Pobierz</a>";
+                    echo "</div>";
+                }
+            }
+            mysqli_close($polaczenie);
+        ?>
     </section>
     <section id="prawy">
         <h2>Najbardziej lubiane</h2>
         <!-- skrypt 2 -->
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'galeria');
+            if(!$polaczenie){
+                exit();
+            }
+            else{
+                $zapytanie = "SELECT tytul, plik FROM `zdjecia` WHERE polubienia >= 100;";
+                $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                while($tab = mysqli_fetch_array($odpowiedz)){
+                    echo "<img src='$tab[1]' alt='$tab[0]'>";
+                }
+            }
+            mysqli_close($polaczenie);
+        ?>
         <strong>Zobacz wszystkie nasze zdjęcia</strong>
     </section>
     <footer>
