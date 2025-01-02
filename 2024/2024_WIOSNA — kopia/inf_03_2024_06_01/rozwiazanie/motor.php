df92dcda4a1a60d5bfbcf07c91bfa3db9f288dc3
Szymon Maniak <szymekmaniak075@gmail.com>
2025-01-02 18:02:06 +0100

    zip

--- 2024/2024_WIOSNA — kopia/inf_03_2024_06_01/rozwiazanie/motor.php
+++ 2024/2024_WIOSNA — kopia/inf_03_2024_06_01/rozwiazanie/motor.php
@@ -15,29 +15,56 @@
         <h2>Gdzie pojechać?</h2>
         <dl>
             <!-- skrypt 1 -->
+            <?php
+                $polaczenie = mysqli_connect('localhost', 'root', '' , 'motory');
+                if(!$polaczenie){
+                    exit();
+                }
+                else{
+                    $zapytanie = "SELECT nazwa, opis, poczatek, zdjecia.zrodlo FROM `wycieczki` JOIN zdjecia ON zdjecia_id = zdjecia.id;";
+                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                    while($tab = mysqli_fetch_array($odpowiedz)){
+                        echo "<dt>$tab[0], rozpoczyna się $tab[2], <a href='$tab[3]'>zobacz zdjęcie</a></dt>";
+                        echo "<dd>$tab[1]</dd>";
+                    }
+                }
+                mysqli_close($polaczenie);
+            ?>
         </dl>
     </article>
-    <aside>
-        <section id="prawy1">
-            <h2>Co kupić?</h2>
-            <ol>
-                <li>Honda CBR125R</li>
-                <li>Yamaha YBR125</li>
-                <li>Honda VFR800i</li>
-                <li>Honda CBR1100XX</li>
-                <li>BMW R1200GS LC</li>
-            </ol>
-        </section>
-        <section id="prawy2">
-            <h2>Statystyki</h2>
-            <p>
-                Wpisanych wycieczek:
-                <!-- skrypt 2 -->
-            </p>
-            <p>Użytkowników forum: 200</p>
-            <p>Przesłanych zdjęć: 1300</p>
-        </section>
-    </aside>
+    <section id="prawy1">
+        <h2>Co kupić?</h2>
+        <ol>
+            <li>Honda CBR125R</li>
+            <li>Yamaha YBR125</li>
+            <li>Honda VFR800i</li>
+            <li>Honda CBR1100XX</li>
+            <li>BMW R1200GS LC</li>
+        </ol>
+    </section>
+    <section id="prawy2">
+        <h2>Statystyki</h2>
+        <p>
+            Wpisanych wycieczek:
+            <!-- skrypt 2 -->
+            <?php
+                $polaczenie = mysqli_connect('localhost', 'root', '' , 'motory');
+                if(!$polaczenie){
+                    exit();
+                }
+                else{
+                    $zapytanie = "SELECT COUNT(*) FROM `wycieczki` WHERE 1;";
+                    $odpowiedz = mysqli_query($polaczenie, $zapytanie);
+                    while($tab = mysqli_fetch_array($odpowiedz)){
+                        echo "<p>Wpisanych wycieczek: $tab[0]</p>";
+                    }
+                }
+                mysqli_close($polaczenie);
+            ?>
+        </p>
+        <p>Użytkowników forum: 200</p>
+        <p>Przesłanych zdjęć: 1300</p>
+    </section>
     <footer>
         <p>Stronę wykonał: Szymon Maniak 5TI</p>
     </footer>
