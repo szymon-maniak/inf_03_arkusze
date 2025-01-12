78848e552f397f6efa940f445495adff0ae2b0dd
Szymon Maniak <szymekmaniak075@gmail.com>
2025-01-12 18:03:27 +0100

    js

---
+++ 2024/2024_WIOSNA - version - 3/inf_03_2024_06_08/rozwiazanie/skrypt.js
@@ -0,0 +1,29 @@
+function aktywacja(blok){
+    document.getElementById('blok1').style.display = "none";
+    document.getElementById('blok2').style.display = "none";
+    document.getElementById('blok3').style.display = "none";
+    if(blok == 1) document.getElementById('blok1').style.display = "block";
+    else if(blok == 2) document.getElementById('blok2').style.display = "block";
+    else if(blok == 3) document.getElementById('blok3').style.display = "block";
+}
+
+let szerokosc = 4;
+function zwiekszPasek(){
+    if(szerokosc < 100){
+        szerokosc += 12;
+        document.getElementById('pasek').style.width = szerokosc + "%";
+    }
+}
+
+function zatwierdzenie(){
+    let imie = document.getElementById('imie').value;
+    let nazwisko = document.getElementById('nazwisko').value;
+    let data_urodzenia = document.getElementById('data_urodzenia').value;
+    let ulica = document.getElementById('ulica').value;
+    let numer = document.getElementById('numer').value;
+    let miasto = document.getElementById('miasto').value;
+    let numer_komorkowy = document.getElementById('numer_komorkowy').value;
+    let rodo = document.getElementById('rodo').checked;
+
+    console.log(imie + ", " + nazwisko + ", " + data_urodzenia + ", " + ulica + ", " + numer + ", " + miasto + ", " + numer_komorkowy + ", " + rodo);
+}
